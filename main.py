939f8b2c7ad8388a690aae229fee88ea7a8db672
Harsh Balgude <140716597+b-harsh274@users.noreply.github.com>
2024-08-08 01:34:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import os
 from train import Trainer
 
 def main():
-    signal = 'path/to/signal'
-    label = 'path/to/labels'
+    signal_path = 'path/to/signal'
+    label_path = 'path/to/labels'
     output_path = 'path/to/output'
 
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
     trainer = Trainer(output_path)
-    trainer.train_ecg(signal, label)
+    trainer.train_ecg(signal_path, label_path)
 
 if __name__ == "__main__":
     main()
